c12d789863c0a6f624f91c5cdb591d68f713fae7
Andrew Robinson <andrew0robinson@yahoo.co.uk>
2021-05-11 11:14:57 +0100

    pushing latest progress

---
+++ src/app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const app = express();
+
+app.use(express.json());
+
+module.exports = app;
+
+app.get('/', (req, res) => {
+    res.status(200).send('Hello World');
+})
