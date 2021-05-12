5622648a0777ac0c903b8828992ed313c497c226
Andrew Robinson <andrew0robinson@yahoo.co.uk>
2021-05-12 18:08:59 +0100

    added first test

--- src/app.js
+++ src/app.js
@@ -1,11 +1,9 @@
 const express = require('express');
+const artistRouter = require('./routes/artist');
 
 const app = express();
 
 app.use(express.json());
+app.use('/artist', artistRouter);
 
 module.exports = app;
-
-app.get('/', (req, res) => {
-    res.status(200).send('Hello World');
-})
